0e8d0995bd872cfa75922eaec27ece85ea968c5f
Mateus Silva <gmateusjose6@gmail.com>
2022-04-25 15:12:30 -0300

    reseting again

--- main.py
+++ main.py
@@ -4,60 +4,64 @@ import os
 from PIL import Image
 
 
-MAX_FILESIZE = 10**6  # bytes
+UHD_HEIGHT = 2160  # Amount of y-pixels in Ultra HD images
+MAX_FILESIZE = 10**6  # Maximum filesize of image output
 MIN_QUALITY = 1  # Minimum quality
 MAX_QUALITY = 95 # Maximum quality
 
 
-def compress(filepath, quality):
-    filename = os.path.basename(filepath)
-    compressed_filename = os.path.join('cmp', filename)
-
-    with Image.open(filepath) as img:
-        print(f'saving {compressed_filename} with quality {quality}')
-        img.save(compressed_filename, optimized=True, quality=quality)
-
-    return os.path.getsize(compressed_filename)
+def check_image_status(path):
+    with Image.open(path) as img:
+        print(f"[+] {path} {img.size[0]}x{img.size[1]} {os.path.getsize(path)} bytes")
 
 
 def main():
-    filepath = sys.argv[1]
-    if (size := os.path.getsize(filepath)) < MAX_FILESIZE:
-        print(f'{filepath} {size} bytes does not need a compression')
+    input = sys.argv[1]
+    filename = os.path.basename(input)
+    output = os.path.join('cmp', filename)
+
+    check_image_status(input)
+
+    if (size := os.path.getsize(input)) < MAX_FILESIZE:
+        print(f'{input} {size} bytes does not need a compression')
         return
 
-    possible_qualities = [i for i in range(MIN_QUALITY, MAX_QUALITY + 1)]
+    with Image.open(input) as img:
+        _, img_height = img.size
+        if img_height > UHD_HEIGHT:
+            new_img = img.reduce(int(img_height / UHD_HEIGHT))
+            new_img.save(output)
+            input = output
 
-    if sys.argv[2] == 'linear':
-        linear_search(filepath, possible_qualities)
-    elif sys.argv[2] == 'binary':
-        binary_search(filepath, possible_qualities)
-    else:
-        print('USAGE: python main.py [filename] [linear | binary]')
+    check_image_status(output)
 
-
-def linear_search(filepath, possible_qualities):
-    for quality in reversed(possible_qualities):
-        if (filesize := compress(filepath, quality)) < MAX_FILESIZE:
-            print(f'{filepath} compressed to {filesize} bytes')
-            break
-
-
-def binary_search(filepath, possible_qualities):
+    available_qualities = [q for q in range(MIN_QUALITY, MAX_QUALITY + 1)]
     min_quality_index = 0
-    max_quality_index = len(possible_qualities) - 1
+    max_quality_index = len(available_qualities) - 1
+
+    last_quality_used = None
     while True:
         mid_quality_index = int((min_quality_index + max_quality_index) / 2)
-        filesize = compress(filepath, possible_qualities[mid_quality_index])
+        quality = available_qualities[mid_quality_index]
 
-        if filesize > MAX_FILESIZE:
+        if (last_quality_used is not None) and (last_quality_used == 1) and quality == 1:
+            print('Could not further compress')
+            break
+
+        with Image.open(input) as img:
+            img.save(output, optimized=True, quality=quality)
+            check_image_status(output)
+
+        if (filesize := os.path.getsize(output)) > MAX_FILESIZE:
             max_quality_index = mid_quality_index
         elif (filesize < MAX_FILESIZE) and (min_quality_index < mid_quality_index):
             min_quality_index = mid_quality_index
         else:
-            print(f'{filepath} compressed to {filesize} bytes')
+            print(f'[+] {output} compressed to {filesize} bytes')
             break
 
+        last_quality_used = quality
+        input = output
 
 if __name__ == '__main__':
     main()

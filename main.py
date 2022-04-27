f39603bbe1a459c7e26c3eab7ebcbab08f963914
Mateus Silva <gmateusjose6@gmail.com>
2022-04-27 16:27:36 -0300

    refactoring compression

--- main.py
+++ main.py
@@ -1,64 +1,65 @@
+import base64
 import sys
 import os
+import tempfile
 
 from PIL import Image
 
 
 UHD_HEIGHT = 2160  # Amount of y-pixels in Ultra HD images
 MAX_FILESIZE = 10**6  # Maximum filesize of image output
-MIN_QUALITY = 1  # Minimum quality
-MAX_QUALITY = 95 # Maximum quality
-
-
-def check_image_status(path):
-    with Image.open(path) as img:
-        print(f"[+] {path} {img.size[0]}x{img.size[1]} {os.path.getsize(path)} bytes")
+MIN_QUALITY, MAX_QUALITY = 1, 95 # Quality configuration
 
 
 def main():
     input = sys.argv[1]
+    with open(input, 'rb') as f:
+        doc = generate_compressed_base64(f)
+
+    _save_b64(input, doc)
+
+
+def generate_compressed_base64(file):
+    file.seek(0, os.SEEK_END)
+    if file.tell() < MAX_FILESIZE:
+        file.seek(0)
+        return base64.b64encode(file.read()).decode('utf-8')
+
+    with tempfile.TemporaryFile() as filecopy:
+        # Resizing UHD Images
+        with Image.open(file) as img:
+            _, img_height = img.size
+            if img_height > UHD_HEIGHT:
+                new_img = img.reduce(int(img_height / UHD_HEIGHT))
+                new_img.save(filecopy, format=img.format)
+            else:
+                img.save(filecopy, format=img.format)
+
+        filecopy.seek(0, os.SEEK_END)
+        if filecopy.tell() < MAX_FILESIZE:
+            filecopy.seek(0)
+            return base64.b64encode(filecopy.read()).decode('utf-8')
+
+        # Reducing image quality
+        for quality in reversed(range(MIN_QUALITY, MAX_QUALITY + 1)):
+            with Image.open(filecopy) as img:
+                img.save(filecopy, format=img.format, optimize=True, quality=quality)
+
+            filecopy.seek(0, os.SEEK_END)
+            if filecopy.tell() < MAX_FILESIZE:
+                filecopy.seek(0)
+                return base64.b64encode(filecopy.read()).decode('utf-8')
+
+    raise Exception('Could not further compress')
+
+
+def _save_b64(input, base64_utf8_string):
     filename = os.path.basename(input)
-    output = os.path.join('cmp', filename)
+    output = os.path.join('b64', f'{os.path.splitext(filename)[0]}.txt')
 
-    check_image_status(input)
+    with open(output, 'wt') as f:
+        f.write(base64_utf8_string)
 
-    if (size := os.path.getsize(input)) < MAX_FILESIZE:
-        print(f'{input} {size} bytes does not need a compression')
-        return
-
-    with Image.open(input) as img:
-        _, img_height = img.size
-        if img_height > UHD_HEIGHT:
-            new_img = img.reduce(int(img_height / UHD_HEIGHT))
-            new_img.save(output)
-            input = output
-
-    available_qualities = [q for q in range(MIN_QUALITY, MAX_QUALITY + 1)]
-    min_quality_index = 0
-    max_quality_index = len(available_qualities) - 1
-
-    last_quality_used = None
-    while True:
-        mid_quality_index = int((min_quality_index + max_quality_index) / 2)
-        quality = available_qualities[mid_quality_index]
-
-        if (last_quality_used is not None) and (last_quality_used == 1) and quality == 1:
-            print('Could not further compress')
-            break
-
-        with Image.open(input) as img:
-            img.save(output, optimized=True, quality=quality)
-
-        if (filesize := os.path.getsize(output)) > MAX_FILESIZE:
-            max_quality_index = mid_quality_index
-        elif (filesize < MAX_FILESIZE) and (min_quality_index < mid_quality_index):
-            min_quality_index = mid_quality_index
-        else:
-            print(f'[+] {output} compressed to {filesize} bytes')
-            break
-
-        last_quality_used = quality
-        input = output
 
 if __name__ == '__main__':
     main()
